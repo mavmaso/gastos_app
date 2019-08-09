4e0ad2de126808bf0c7b020052ded4990e7a0edc
Marcos Soares <mvmsoares@gmail.com>
2019-08-08 22:58:50 -0300

    pre config

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gastos App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gastos App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
+      ),
+    );
+  }
+}
